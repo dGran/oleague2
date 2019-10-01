107b641955b9fea0f90d5d26ec2a9732c8751edf
David Gran <dgran@MBP-DGran.local>
2019-10-01 23:16:18 +0200

    show more info in market teams view and show market periods in market header

--- resources/views/market/partials/header.blade.php
+++ resources/views/market/partials/header.blade.php
@@ -8,8 +8,14 @@
     			Mercado de fichajes
     		</h3>
     		<span class="subtitle">
-    			{{ active_season()->name }}
+    			PES2020 / {{ active_season()->name }}
     		</span>
+			<div class="market-state">
+				<span class="badge badge-{{ active_season()->change_salaries_period ? 'success' : 'secondary' }}">Salarios</span>
+				<span class="badge badge-{{ active_season()->transfers_period ? 'success' : 'secondary' }}">Transfers</span>
+				<span class="badge badge-{{ active_season()->free_players_period ? 'success' : 'secondary' }}">Libres</span>
+				<span class="badge badge-{{ active_season()->clausules_period ? 'success' : 'secondary' }}">Claúsulas</span>
+			</div>
 		</div>
 	</div>
 </div>
--- resources/views/market/teams/card_data.blade.php
+++ resources/views/market/teams/card_data.blade.php
@@ -10,9 +10,24 @@
 				{{ $participant->sub_name() }}
 			</span> {{-- name --}}
 		</div> {{-- title --}}
-		<i class="budget-icon icon-safe-box"></i>
-		<div class="budget">
-			{{ number_format($participant->budget(), 2, ',', '.') }} <span class="measure">mill.</span>
+		{{-- <i class="budget-icon icon-safe-box"></i> --}}
+		<div class="budget text-right">
+			<div class="d-block">
+				<small class="text-muted mr-2">PRESUPUESTO</small>
+				{{ number_format($participant->budget(), 2, ',', '.') }} <span class="measure">M.</span>
+			</div>
+			<div class="d-block mt-2">
+				{{ $participant->players->count() }}
+				<small class="text-muted ml-1">JUGADORES</small>
+			</div>
+			<div class="d-block">
+				<small class="text-muted mr-2">CLAUSULAS PAGADAS</small>
+				{{ $participant->paid_clauses }} / {{ active_season()->max_clauses_paid }}
+			</div>
+			<div class="d-block">
+				<small class="text-muted mr-2">CLAUSULAS RECIBIDAS</small>
+				{{ $participant->clauses_received }} / {{ active_season()->max_clauses_received }}
+			</div>
 		</div> {{-- budget --}}
 	</div> {{-- item --}}
 </a>
