459f400e0a673561cdbebaee128cdd80688db71c
David Gran <dgran@MBP-DGran.local>
2019-10-16 03:01:42 +0200

    rules view update

--- resources/views/rules/header.blade.php
+++ resources/views/rules/header.blade.php
@@ -1,9 +1,9 @@
 <div class="club-header">
 	<div class="container py-2">
-		<div style="display: table-cell;vertical-align: middle;">
+		<div class="d-table-cell align-middle">
 			<i class="icon-judge" style="background: #fff; border-radius: 50%; color: #be0346; display: block; font-size: 3em; padding: 12px;"></i>
 		</div>
-		<div class="d-table-cell pl-2 align-middle">
+		<div class="d-table-cell pl-3 align-middle">
     		<h3>
     			Reglamento
     		</h3>
