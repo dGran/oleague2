07e33b50889afeaf85bc63cbb2a623df65c7ecb5
dgran <dgranh@gmail.com>
2020-01-21 04:40:12 +0100

    competition menu add phase and group

--- resources/views/competitions/partials/menu.blade.php
+++ resources/views/competitions/partials/menu.blade.php
@@ -2,14 +2,14 @@
 	<div class="scrolling-wrapper">
 		<ul>
 			<li class="item {{ \Route::current()->getName() == 'competitions.table' ? 'active' : '' }}">
-				<a href="{{ route('competitions.table', [active_season()->slug, $group->phase->competition->slug]) }}">
+				<a href="{{ route('competitions.table', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 					<i class="fas fa-th-list"></i>
 					<span>Clasificación</span>
 				</a>
 			</li>
 
 			<li class="item {{ \Route::current()->getName() == 'competitions.calendar' ? 'active' : '' }}">
-				<a href="{{ route('competitions.calendar', [active_season()->slug, $group->phase->competition->slug]) }}">
+				<a href="{{ route('competitions.calendar', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 					<i class="icon-soccer-ball"></i>
 					<span>Partidos</span>
 				</a>
@@ -17,7 +17,7 @@
 
 			@if ($league->has_stats())
 				<li class="item {{ \Route::current()->getName() == 'competitions.stats' ? 'active' : '' }}">
-					<a href="{{ route('competitions.stats', [active_season()->slug, $group->phase->competition->slug]) }}">
+					<a href="{{ route('competitions.stats', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 						<i class="fas fa-chart-bar"></i>
 						<span>Estadísticas</span>
 					</a>
@@ -29,14 +29,14 @@
 	<div class="scrolling-wrapper">
 		<ul>
 			<li class="item {{ \Route::current()->getName() == 'competitions.table' ? 'active' : '' }}">
-				<a href="{{ route('competitions.table', [active_season()->slug, $group->phase->competition->slug]) }}">
+				<a href="{{ route('competitions.table', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 					<i class="icon-playoff"></i>
 					<span>Playoffs</span>
 				</a>
 			</li>
 
 			<li class="item {{ \Route::current()->getName() == 'competitions.calendar' ? 'active' : '' }}">
-				<a href="{{ route('competitions.calendar', [active_season()->slug, $group->phase->competition->slug]) }}">
+				<a href="{{ route('competitions.calendar', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 					<i class="icon-soccer-ball"></i>
 					<span>Partidos</span>
 				</a>
@@ -44,7 +44,7 @@
 
 			@if ($playoff->has_stats())
 				<li class="item {{ \Route::current()->getName() == 'competitions.stats' ? 'active' : '' }}">
-					<a href="{{ route('competitions.stats', [active_season()->slug, $group->phase->competition->slug]) }}">
+					<a href="{{ route('competitions.stats', [active_season()->slug, $group->phase->competition->slug, $group->phase->slug, $group->slug]) }}">
 						<i class="fas fa-chart-bar"></i>
 						<span>Estadísticas</span>
 					</a>
