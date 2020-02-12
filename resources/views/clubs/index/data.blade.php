b8f5e69aa378a871d68db0d677721b8f664c2523
dgran <dgranh@gmail.com>
2020-02-12 23:39:43 +0100

    clubs list add hover style

--- resources/views/clubs/index/data.blade.php
+++ resources/views/clubs/index/data.blade.php
@@ -3,7 +3,7 @@
 		@foreach ($participants as $participant)
 			@if ($participant->team)
 				<div class="col-12 col-md-6 col-lg-4">
-					<div class="club-card border" style="background: #fff; margin: 15px 5px 0 5px; padding: 1em 0">
+					<div class="club-card">
 						<div class="text-center d-table-cell" style="width: 170px">
 							<a class="text-dark" href="{{route('club', $participant->team->slug) }}">
 								<img src="{{ $participant->logo() }}" alt="" width="72px">
@@ -12,7 +12,7 @@
 							</a>
 						</div>
 						<div class="d-table-cell border-left align-top ">
-							<ul style="font-size: .9em; padding: 0 1em; list-style: none; line-height: 1.4rem">
+							<ul>
 								<li>
 									<a class="text-dark" href="{{route('club', $participant->team->slug) }}">
 										<i class="fas fa-caret-right mr-1" style="color: #89be38"></i>
