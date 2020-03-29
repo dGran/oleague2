4ba8217f02bf03d47f4487a67c511b20d214d563
dgran <dgranh@gmail.com>
2020-03-29 09:18:14 +0200

    add player details in competition stats - add info participant

--- resources/views/competitions/league/stats/goals.blade.php
+++ resources/views/competitions/league/stats/goals.blade.php
@@ -26,6 +26,23 @@
 	</div>
 @endif
 
+@if ($stats_goals->count() > 0 && $participant_id > 0)
+	<div class="row justify-content-center">
+		<div class="col-12 col-md-10 col-lg-8 px-3 px-md-0 py-3 animated fadeIn">
+			<div class="d-inline-block align-middle">
+				<figure class="bg-white border rounded-circle m-0 shadow" style="padding: 10px">
+					<img src="{{ $participant->logo() }}" width="40">
+				</figure>
+			</div>
+			<div class="d-inline-block align-middle pl-2">
+				<strong>{{ $participant->name() }}</strong>
+				<small class="text-muted d-block">
+					{{ $participant->sub_name() }}
+				</small>
+			</div>
+		</div>
+	</div>
+@endif
 <div class="row justify-content-center">
 	<div class="col-12 col-md-10 col-lg-8 px-0 py-3">
 		<div class="px-3 px-md-0 pb-2">
