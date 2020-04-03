3b3ed455e019102e5a67ba1c9dedb8eddc79dda3
dgran <dgranh@gmail.com>
2020-04-03 11:40:27 +0200

    fix z-index bottom-fixed, fix query pending matches, add empty list in calendar and pending matches

--- resources/views/clubs/calendar/data.blade.php
+++ resources/views/clubs/calendar/data.blade.php
@@ -6,79 +6,85 @@
 
 <div class="container p-3">
 	<div class="calendar">
-	@foreach ($matches as $match)
-		<div class="item shadow-sm" onmouseenter="item_enter({{$match->id}})" onmouseleave="item_leave({{$match->id}})">
-			@if (!is_null($match->day_id))
-				@if ($match->date_limit > $match->day->date_limit)
-					<div class="ribbon left info"><span>Plazo ampliado</span></div>
-				@endif
-			@else
-				@if ($match->date_limit > $match->clash->round->date_limit)
-					<div class="ribbon left info"><span>Plazo ampliado</span></div>
-				@endif
-			@endif
-			<div class="row align-items-center">
-				<div class="col local">
-					<img class="team-logo" src="{{ $match->local_participant->participant->logo() }}" alt="">
-					<br>
-					<span class="team-name">{{ $match->local_participant->participant->name() }}</span>
-				</div>
-				<div class="col order-md-first match-info">
-					<div class="competition">
-						<div class="row align-items-center">
-							<div class="col-auto">
-								<img class="logo" src="{{ $match->competition()->getImgFormatted() }}" alt="" width="18">
+		@if ($matches->count() > 0)
+			@foreach ($matches as $match)
+				<div class="item shadow-sm" onmouseenter="item_enter({{$match->id}})" onmouseleave="item_leave({{$match->id}})">
+					@if (!is_null($match->day_id))
+						@if ($match->date_limit > $match->day->date_limit)
+							<div class="ribbon left info"><span>Plazo ampliado</span></div>
+						@endif
+					@else
+						@if ($match->date_limit > $match->clash->round->date_limit)
+							<div class="ribbon left info"><span>Plazo ampliado</span></div>
+						@endif
+					@endif
+					<div class="row align-items-center">
+						<div class="col local">
+							<img class="team-logo" src="{{ $match->local_participant->participant->logo() }}" alt="">
+							<br>
+							<span class="team-name">{{ $match->local_participant->participant->name() }}</span>
+						</div>
+						<div class="col order-md-first match-info">
+							<div class="competition">
+								<div class="row align-items-center">
+									<div class="col-auto">
+										<img class="logo" src="{{ $match->competition()->getImgFormatted() }}" alt="" width="18">
+									</div>
+									<div class="col-auto align-middle">
+										<span class="name">{{ $match->match_name_array()['competition'] }}</span>
+										@if (isset($match->match_name_array()['group']))
+											<span class="group">{{ $match->match_name_array()['group'] }}</span>
+										@endif
+										@if ($match->day_id)
+											@if (isset($match->match_name_array()['day']))
+												<span class="day">Jornada {{ $match->match_name_array()['day'] }}</span>
+											@endif
+										@else
+											@if (isset($match->match_name_array()['round']))
+												<span class="round">{{ $match->match_name_array()['round'] }}</span>
+											@endif
+											@if (isset($match->match_name_array()['match']))
+												<span class="match">{{ $match->match_name_array()['match'] }}</span>
+											@endif
+										@endif
+										<span class="date-limit {{ ($match->winner() == -1) ? 'd-block' : 'd-none' }}">
+											<strong>Plazo: </strong>
+											{{ \Carbon\Carbon::parse($match->date_limit_match())->format('j M, H:i') }}</span>
+									</div>
+								</div>
 							</div>
-							<div class="col-auto align-middle">
-								<span class="name">{{ $match->match_name_array()['competition'] }}</span>
-								@if (isset($match->match_name_array()['group']))
-									<span class="group">{{ $match->match_name_array()['group'] }}</span>
-								@endif
-								@if ($match->day_id)
-									@if (isset($match->match_name_array()['day']))
-										<span class="day">Jornada {{ $match->match_name_array()['day'] }}</span>
-									@endif
+							@if (!($match->winner() == -1))
+								<a class="d-inline-block d-md-none btn btn-link result" href="{{ route("competitions.calendar.match.details", [$season->slug, $match->competition()->slug, $match->id]) }}" onclick="view_match_detail(this)">
+									{{ $match->local_score }} - {{ $match->visitor_score }}
+								</a>
+							@endif
+						</div>
+						<div class="col-md-auto d-none d-md-block vs">
+								@if ($match->winner() == -1)
+									<strong class="blank-result">Vs</strong>
 								@else
-									@if (isset($match->match_name_array()['round']))
-										<span class="round">{{ $match->match_name_array()['round'] }}</span>
-									@endif
-									@if (isset($match->match_name_array()['match']))
-										<span class="match">{{ $match->match_name_array()['match'] }}</span>
-									@endif
+									<a href="{{ route("competitions.calendar.match.details", [$season->slug, $match->competition()->slug, $match->id]) }}" class="btn btn-link result" onclick="view_match_detail(this)">
+										{{ $match->local_score }} - {{ $match->visitor_score }}
+									</a>
 								@endif
-								<span class="date-limit {{ ($match->winner() == -1) ? 'd-block' : 'd-none' }}">
-									<strong>Plazo: </strong>
-									{{ \Carbon\Carbon::parse($match->date_limit_match())->format('j M, H:i') }}</span>
-							</div>
+						</div>
+						<div class="col visitor">
+							<img class="team-logo" src="{{ $match->visitor_participant->participant->logo() }}" alt="">
+							<br>
+							<span class="team-name">{{ $match->visitor_participant->participant->name() }}</span>
 						</div>
 					</div>
-					@if (!($match->winner() == -1))
-						<a class="d-inline-block d-md-none btn btn-link result" href="{{ route("competitions.calendar.match.details", [$season->slug, $match->competition()->slug, $match->id]) }}" onclick="view_match_detail(this)">
-							{{ $match->local_score }} - {{ $match->visitor_score }}
+					<div class="competition-link" id="competition_link{{$match->id}}">
+						<a href="{{ route('competitions.calendar', [$season->slug, $match->competition()->slug, $match->group()->phase_slug_if_necesary(), $match->group()->group_slug_if_necesary()]) }}" class="btn btn-link form-control">
+							{{ $match->competition()->name }}
 						</a>
-					@endif
+					</div>
 				</div>
-				<div class="col-md-auto d-none d-md-block vs">
-						@if ($match->winner() == -1)
-							<strong class="blank-result">Vs</strong>
-						@else
-							<a href="{{ route("competitions.calendar.match.details", [$season->slug, $match->competition()->slug, $match->id]) }}" class="btn btn-link result" onclick="view_match_detail(this)">
-								{{ $match->local_score }} - {{ $match->visitor_score }}
-							</a>
-						@endif
-				</div>
-				<div class="col visitor">
-					<img class="team-logo" src="{{ $match->visitor_participant->participant->logo() }}" alt="">
-					<br>
-					<span class="team-name">{{ $match->visitor_participant->participant->name() }}</span>
-				</div>
+			@endforeach
+		@else
+			<div class="empty">
+				No existen partidos
 			</div>
-			<div class="competition-link" id="competition_link{{$match->id}}">
-				<a href="{{ route('competitions.calendar', [$season->slug, $match->competition()->slug, $match->group()->phase_slug_if_necesary(), $match->group()->group_slug_if_necesary()]) }}" class="btn btn-link form-control">
-					{{ $match->competition()->name }}
-				</a>
-			</div>
-		</div>
-	@endforeach
+		@endif
 	</div>
 </div>
--- resources/views/clubs/pending_matches/data.blade.php
+++ resources/views/clubs/pending_matches/data.blade.php
@@ -6,78 +6,73 @@
 
 <div class="container p-3">
 	<div class="calendar">
-	@foreach ($matches as $match)
-		@if ($match->winner() == -1 && $match->active)
-			<div class="item shadow-sm {{ $match->date_limit_match() && $match->date_limit_match() < now() ? 'time-limit-exceded' : '' }}" onmouseenter="item_enter({{$match->id}})" onmouseleave="item_leave({{$match->id}})">
-				@if ($match->date_limit_match())
-					@if ($match->date_limit_match() < now())
-						<div class="ribbon left danger"><span>Plazo vencido</span></div>
+		@if ($matches->count() > 0)
+			@foreach ($matches as $match)
+				<div class="item shadow-sm {{ $match->date_limit_match() && $match->date_limit_match() < now() ? 'time-limit-exceded' : '' }}" onmouseenter="item_enter({{$match->id}})" onmouseleave="item_leave({{$match->id}})">
+					@if ($match->date_limit_match())
+						@if ($match->date_limit_match() < now())
+							<div class="ribbon left danger"><span>Plazo vencido</span></div>
+						@endif
+					@else
+						<div class="ribbon info">Plazo N/D</span>
 					@endif
-				@else
-					<div class="ribbon info">Plazo N/D</span>
-				@endif
 
-				<div class="row align-items-center">
-					<div class="col local">
-						<img class="team-logo" src="{{ $match->local_participant->participant->logo() }}" alt="">
-						<br>
-						<span class="team-name">{{ $match->local_participant->participant->name() }}</span>
-					</div>
-					<div class="col order-md-first match-info">
-						<div class="competition">
-							<div class="row align-items-center">
-								<div class="col-auto">
-									<img class="logo" src="{{ $match->competition()->getImgFormatted() }}" alt="" width="18">
-								</div>
-								<div class="col-auto align-middle">
-									<span class="name">{{ $match->match_name_array()['competition'] }}</span>
-									@if (isset($match->match_name_array()['group']))
-										<span class="group">{{ $match->match_name_array()['group'] }}</span>
-									@endif
-									@if ($match->day_id)
-										@if (isset($match->match_name_array()['day']))
-											<span class="day">Jornada {{ $match->match_name_array()['day'] }}</span>
+					<div class="row align-items-center">
+						<div class="col local">
+							<img class="team-logo" src="{{ $match->local_participant->participant->logo() }}" alt="">
+							<br>
+							<span class="team-name">{{ $match->local_participant->participant->name() }}</span>
+						</div>
+						<div class="col order-md-first match-info">
+							<div class="competition">
+								<div class="row align-items-center">
+									<div class="col-auto">
+										<img class="logo" src="{{ $match->competition()->getImgFormatted() }}" alt="" width="18">
+									</div>
+									<div class="col-auto align-middle">
+										<span class="name">{{ $match->match_name_array()['competition'] }}</span>
+										@if (isset($match->match_name_array()['group']))
+											<span class="group">{{ $match->match_name_array()['group'] }}</span>
 										@endif
-									@else
-										@if (isset($match->match_name_array()['round']))
-											<span class="round">{{ $match->match_name_array()['round'] }}</span>
+										@if ($match->day_id)
+											@if (isset($match->match_name_array()['day']))
+												<span class="day">Jornada {{ $match->match_name_array()['day'] }}</span>
+											@endif
+										@else
+											@if (isset($match->match_name_array()['round']))
+												<span class="round">{{ $match->match_name_array()['round'] }}</span>
+											@endif
+											@if (isset($match->match_name_array()['match']))
+												<span class="match">{{ $match->match_name_array()['match'] }}</span>
+											@endif
 										@endif
-										@if (isset($match->match_name_array()['match']))
-											<span class="match">{{ $match->match_name_array()['match'] }}</span>
-										@endif
-									@endif
-									<span class="date-limit">
-										<strong>Plazo: </strong>
-										{{ \Carbon\Carbon::parse($match->date_limit_match())->format('j M, H:i') }}</span>
+										<span class="date-limit">
+											<strong>Plazo: </strong>
+											{{ \Carbon\Carbon::parse($match->date_limit_match())->format('j M, H:i') }}</span>
+									</div>
 								</div>
 							</div>
 						</div>
+						<div class="col-md-auto d-none d-md-block vs">
+							<strong class="blank-result">Vs</strong>
+						</div>
+						<div class="col visitor">
+							<img class="team-logo" src="{{ $match->visitor_participant->participant->logo() }}" alt="">
+							<br>
+							<span class="team-name">{{ $match->visitor_participant->participant->name() }}</span>
+						</div>
 					</div>
-					<div class="col-md-auto d-none d-md-block vs">
-						<strong class="blank-result">Vs</strong>
-					</div>
-					<div class="col visitor">
-						<img class="team-logo" src="{{ $match->visitor_participant->participant->logo() }}" alt="">
-						<br>
-						<span class="team-name">{{ $match->visitor_participant->participant->name() }}</span>
+					<div class="competition-link" id="competition_link{{$match->id}}">
+						<a href="{{ route('competitions.calendar', [$season->slug, $match->competition()->slug, $match->group()->phase_slug_if_necesary(), $match->group()->group_slug_if_necesary()]) }}" class="btn btn-link form-control">
+							{{ $match->competition()->name }}
+						</a>
 					</div>
 				</div>
-				<div class="competition-link" id="competition_link{{$match->id}}">
-					<a href="{{ route('competitions.calendar', [$season->slug, $match->competition()->slug, $match->group()->phase_slug_if_necesary(), $match->group()->group_slug_if_necesary()]) }}" class="btn btn-link form-control">
-						{{ $match->competition()->name }}
-					</a>
-				</div>
-
-{{--             	<div class="bottom-data text-center py-3">
-            		@if ($match->date_limit_match())
-						<small class="text-muted d-block">
-							<strong class="mr-1">Fecha Límite</strong>
-							{{ \Carbon\Carbon::parse($match->date_limit_match())->format('j M, H:i') }}
-						</small>
-					@endif
-				</div> --}}
+			@endforeach
+		@else
+			<div class="empty">
+				No existen partidos pendientes
 			</div>
 		@endif
-	@endforeach
 	</div>
 </div>
