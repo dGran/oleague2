c53f0c7180a5c54baf8f380dcd71a5f67fa96752
dgran <dgranh@gmail.com>
2020-04-07 13:38:29 +0200

    change styles in clubs calendar

--- resources/views/clubs/economy/data.blade.php
+++ resources/views/clubs/economy/data.blade.php
@@ -5,34 +5,31 @@
 </div>
 
 <div class="container p-3">
-	@foreach ($participant->cash_history->sortByDesc('created_at') as $cash_history)
-		<div class="economy-item">
-			<div class="description">
-				<small class="text-muted d-block">{{ \Carbon\Carbon::parse($cash_history->created_at)->format('d/m/Y - h:m')}}</small>
-				{{ $cash_history->description }}
-			</div>
-			<div class="data clearfix shadow-sm">
-				<div class="float-left">
-					<div class="type text-muted">
-						@if ($cash_history->movement == "E")
-							<i class="fas fa-piggy-bank text-success mr-1"></i> <small>Entrada</small>
-						@else
-							<i class="fas fa-piggy-bank text-danger mr-1"></i> <small>Salida</small>
-						@endif
-					</div>
+	<div class="economy">
+		@foreach ($participant->cash_history->sortByDesc('created_at') as $cash_history)
+			<div class="item shadow-sm">
+				<div class="date text-muted">{{ \Carbon\Carbon::parse($cash_history->created_at)->format('d/m/Y - h:m')}}</div>
+				<div class="description">
+					<i class="fas fa-piggy-bank mr-2 {{ $cash_history->movement == "E" ? 'text-success' : 'text-danger' }}"></i>
+					{{ $cash_history->description }}
 				</div>
-				<div class="float-right">
-					<div class="amount">
-						@if ($cash_history->movement == "S")
-						-
-						@endif
-						{{ $cash_history->amount }} M
+				<div class="data clearfix {{ $cash_history->movement == "E" ? 'money-in' : 'money-out' }}">
+					<div class="float-left d-inline-block d-md-none">
+						<div class="type text-muted">
+							<i class="fas fa-piggy-bank mr-1 {{ $cash_history->movement == "E" ? 'text-success' : 'text-danger' }}"></i>
+							<span>{{ $cash_history->movement == "E" ? 'Entrada' : 'Salida' }}</span>
+						</div>
 					</div>
-					<div class="cash text-muted" style="min-width: 80px">
-						<small>Caja: {{ $cash_history->cash }} M</small>
+					<div class="float-right">
+						<div class="amount">
+							{{ $cash_history->movement == "S" ? '-' : '' }}{{ $cash_history->amount }} M
+						</div>
+						<div class="cash text-muted" style="min-width: 80px">
+							<small>Caja: {{ $cash_history->cash }} M</small>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	@endforeach
+		@endforeach
+	</div>
 </div>
